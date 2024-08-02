0a30b62deda4636477276cb90de6fdc0e9b02bb0
Geethaka Sankalpa <geethaka.20231659@iit.ac.lk>
2024-08-03 00:57:42 +0530

    updated

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-let imgBox = document.getElementById("imgBox");
-let qrImage = document.getElementById("qr");        // image
-let qrText = document.getElementById("qrText");     // input field
+let imgBox = document.getElementById("imgBox");     // gets image Box element
+let qrImage = document.getElementById("qr");        // gets QR image
+let qrText = document.getElementById("qrText");     // gets input field
 
+// Generates a QR code based on the input text
 function generateQR(){
 
+    // Checks if the input text is not empty
     if(qrText.value.length > 0){
+        // Sets the src attribute of the qrImage element to the QR code API URL with the input text as a parameter
         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.ariaValueMax;
         imgBox.classList.add("show-img");
     } else {
         qrText.classList.add("error");
-        setTimeout(()=> {
+        setTimeout(()=> {   // Removes the error class after 100ms
             qrText.classList.remove('error');
         }, 100)
     }
